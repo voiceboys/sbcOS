2440993ceb9378a25191b8d20ac67de38c08492d
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2019-05-10 10:15:08 +0200

    fixed xt_RTPENGINE

--- ISO/gen_sbc_iso.sh
+++ ISO/gen_sbc_iso.sh
@@ -1,1 +1,1 @@
-cd ./sbc-data-15062 && /usr/bin/mkisofs -o  ../sbc-x86_64.iso -v -J -R -D -A sbc -V sbc -no-emul-boot -boot-info-table -boot-load-size 4 -b sbc/boot/isolinux.bin -c sbc/boot/isolinux.boot .
+cd ./sbc-data && /usr/bin/mkisofs -o  ../sbc-x86_64.iso -v -J -R -D -A sbc -V sbc -no-emul-boot -boot-info-table -boot-load-size 4 -b sbc/boot/isolinux.bin -c sbc/boot/isolinux.boot .
